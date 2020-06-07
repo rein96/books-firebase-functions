8206fe74edbdd1d327ea6d44a48247f3279ec1a5
reinhart <reinhartandreas96@gmail.com>
2020-06-07 15:15:11 +0700

    add createAuthor and modify checkAuthentication with admin parameter

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,27 @@ admin.initializeApp();
 const db = admin.firestore();
 const auth = admin.auth();
 
+exports.createAuthor = functions.https.onCall(async (data, context) => {
+    checkAuthentication(context, true);
+  
+    dataValidator(data, {
+      authorName: 'string'
+    })
+  
+    const author = await admin.firestore().collection('authors')
+      .where('name', '==', data.authorName)
+      .limit(1).get();
+  
+    if(!author.empty){
+      throw new functions.https.HttpsError('already-exists',
+        'This author already exists')
+    }
+  
+    return admin.firestore().collection('authors').add({
+      name: data.authorName
+    });
+  })
+
 exports.createPublicProfile = functions.https.onCall(async (data, context) => {
     checkAuthentication(context)
 
@@ -91,14 +112,15 @@ function dataValidator(data, validKeys) {
     }
 }
 
-function checkAuthentication(context) {
-    if (!context.auth) {
-        throw new functions.https.HttpsError(
-            'unauthenticated',
-            'You must be signed in to use this feature'
-        )
+function checkAuthentication(context, admin){
+    if(!context.auth){
+      throw new functions.https.HttpsError('unauthenticated',
+        'You must be signed in to use this feature');
+    }else if(!context.auth.token.admin && admin){
+      throw new functions.https.HttpsError('permission-denied',
+        'You must be an admin to use this feature.');
     }
-}
+  }
 
 // firebase deploy --only functions
 // firebase functions:config:set accounts.admin="admin@gmail.com"
